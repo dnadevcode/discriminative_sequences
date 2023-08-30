eadcff5b033acad969d2bcc681729e65f2416930
Albertas <albertas.dvirnas@thep.lu.se>
2023-08-30 14:43:47 +0200

    upd

--- +Core/extract_species_name.m
+++ +Core/extract_species_name.m
@@ -1,13 +1,18 @@
-function [speciesLevel,idc] = extract_species_name(theoryStruct)
+function [speciesLevel,idc] = extract_species_name(theoryStruct,namesSpecies)
 
+if nargin < 2
+    namesSpecies = {'Escherichia','Shigella'};
+end
 
 % names = [theoryStruct(1:10).name];
 names = arrayfun(@(x) theoryStruct(x).name,1:length(theoryStruct),'un',false);
 
-ecoliTheories = cellfun(@(x) ~isempty(strfind(x,'Escherichia')),names);
-shigellaTheories = cellfun(@(x) ~isempty(strfind(x,'Shigella')),names);
-
-speciesLevel = ecoliTheories+shigellaTheories;
+speciesLevel = zeros(1,length(names));
+for i=1:length(namesSpecies);
+    speciesTheories = cellfun(@(x) ~isempty(strfind(x,namesSpecies{i})),names);
+    speciesLevel = speciesLevel + speciesTheories;
+end
+% speciesLevel = ecoliTheories+shigellaTheories;
 
 if nargout > 1
     % also return unique identifiers for each species
--- discriminative_hca_test.m
+++ discriminative_hca_test.m
@@ -14,6 +14,8 @@ addpath(genpath('/proj/snic2022-5-384/users/x_albdv/data/bargrouping/ecoli/FASTA
 % data loading
 import Core.load_chrom_data;
 [bgAll, bG, kymoStructs] = load_chrom_data('/export/scratch/albertas/data_temp/hca_data/');
+% [bgAll, bG, kymoStructs] = load_chrom_data('/export/scratch/albertas/data_temp/tests/');
+
 fastaFiles(1:length(bG)) = 1;
 
 % nmbp = 0.225;
@@ -43,7 +45,7 @@ w = [200:50:sum(barGenRun{1}.rawBitmask)];
 
 
 import Core.extract_species_name;
-[speciesLevel,idc] = extract_species_name(theoryStruct);
+[speciesLevel,idc] = extract_species_name(theoryStruct,{'Streptococcus'});
 % 
 idx = 1;
 import Core.discrim_true_positives;
@@ -69,10 +71,43 @@ import Core.plot_match_simple;
 [f] = plot_match_simple([barGenRun(selRef) barcodeGenT],rezOut{2}, 1, refNums{selRef}(idx)+1);
 
 
+
+
+%% only single species
+theoryStructSel = theoryStruct(find(speciesLevel));
+barGenRun = bgAll(1);
+w = [];
+[rezMax,bestBarStretch,bestLength,rezOut] = local_alignment_assembly(theoryStructSel, barGenRun,w);
+
+figure,plot(cellfun(@(x) x{1}.maxcoef(1),rezMax))
+w = [200:50:sum(barGenRun{1}.rawBitmask)];
+[rezMax, bestBarStretch, bestLength, rezOut1] = local_alignment_assembly(theoryStructSel, barGenRun,w);
+
 % check nm/bp ratio
 
+
+
 %% nm bp
-    sets.comparisonMethod = 'mass_pcc';
+% Evaluation figure - experiments from individual days
+thryFiles = dir('/export/scratch/albertas/data_temp/bargrouping/New Ref-Theory files May 2022/*.mat');
+
+thryFileIdx = 1;
+sets.thryFile = fullfile(thryFiles(thryFileIdx).folder,thryFiles(thryFileIdx).name);
+
+
+sets.theoryFile{1} = sets.thryFile;
+sets.theoryFileFold{1} = '';
+sets.theory.precision = 5;
+sets.theory.theoryDontSaveTxts = 1;
+import CBT.Hca.UI.Helper.load_theory;
+theoryStructA = load_theory(sets);
+
+
+%% from here independent code
+theoryStructInd =theoryStructA((find(speciesLevel)));
+
+
+sets.comparisonMethod = 'mass_pcc';
 sF = 0.9:0.01:1.1;
 minLen = 300;
 
@@ -80,7 +115,7 @@ minLen = 300;
 %% from here independent code
 % theoryStructInd =theoryStruct([refNums{1}]);
 import CBT.Hca.Core.Analysis.convert_nm_ratio;
-barGenRun = bgAll(7);
+barGenRun = bgAll(1);
 
 % psffac = 1; % scaling factor (in case PSF needs to be something else than 300nm)
 numWorkers = 30; % num workers, in one node there are 30
@@ -89,26 +124,32 @@ sets.comparisonMethod = 'mass_pcc';
 sF = 0.9:0.01:1.1;
 %
 % %     % check for different nmbp (in case estimated incorrectly
-    nmbpvals = 0.15:0.01:0.3; % should use mpMax to get this "fully" correct
-    import Thry.gen_theoretical;
-    scoef = zeros(1,length(nmbpvals));
-    for nmIdx =1:length(nmbpvals)
-            theoryStructAll{nmIdx} = convert_nm_ratio(nmbpvals(nmIdx),theoryStructInd ,sets );
+nmbpvals = 0.15:0.01:0.3; % should use mpMax to get this "fully" correct
+import Thry.gen_theoretical;
+scoef = zeros(1,length(nmbpvals));
+for nmIdx =1:length(nmbpvals)
+    tic
+        theoryStructAll{nmIdx} = convert_nm_ratio(nmbpvals(nmIdx),theoryStructInd ,sets );
 
 %                 [theoryStruct,~,barcodeGenT] = gen_theoretical(fastaFile,nmbpvals(nmIdx),0,nmPerPx); %todo faster: length rscale instead of calculating each time. also this can be done in outside loop. also make it into struct so don't need to load it here and manipulate theory directly
-        [comparisonStruct,rezMax,bestBarStretch] = compare_to_t(barGenRun,  theoryStructAll{nmIdx} ,sF,sets);
-        
-        allCoefs = cellfun(@(x) x.maxcoef(1),comparisonStruct);
-        allLengths = cellfun(@(x) x.lengthMatch (1),comparisonStruct);
-        
+    [comparisonStruct,rezMax,bestBarStretch] = compare_to_t(barGenRun,  theoryStructAll{nmIdx} ,sF,sets);
+    
+    allCoefs = cellfun(@(x) x.maxcoef(1),comparisonStruct);
+    allLengths = cellfun(@(x) x.lengthMatch (1),comparisonStruct);
+    
 %         idxThresh = arrayfun(@(x) find(x<minLen,1,'first'),allLengths);
 %         coefDiffs = allCoefs-mpMaxLenBasedAll{nmIdx}(idxThresh);
-        
-        scoef(nmIdx) = allCoefs;
+    
+    scoef(nmIdx) = allCoefs;
 %         succesNmnp(nmIdx) = length(barsPassThresh)/length(coefDiffs); % this could still have false positives.. ?
 %         succesNmnp(nmIdx) = length(find(cellfun(@(x) x.maxcoef(1),comparisonStruct)-mpMaxLenBasedAll{nmIdx}(idxThresh) > 0.05))/length(bG{idxRun}); % this could still have false positives.. ?
-    end
+    toc
+end
 %     sucRateStruct{idxRun}.succesNmnp = succesNmnp;
 % 
     f = figure
     plot(nmbpvals,scoef);
+
+
+    %%
+    figure,histogram(cellfun(@(x) std(x.rawBarcode),bgAll)./cellfun(@(x) nanmean(std(x.alignedKymo)),bgAll))
