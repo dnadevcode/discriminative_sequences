8818ce1604f1842a5286df9b631f6c2bf1e46997
Albertas Dvirnas <albdvir@gmail.com>
2024-04-29 10:27:43 +0200

    test run

--- test_run.m
+++ test_run.m
@@ -10,3 +10,19 @@ thryFiles = dir('/export/scratch/albertas/download_dump/single/theoryOutput/*.ma
 
 local_pipeline_mp(ix, iy, dirName, depth, windowWidths, sF, thryFiles)
 
+
+
+%%
+% example run:
+ix=2
+iy=1
+dirName = '/proj/snic2022-5-384/users/x_albdv/data/discriminative/pyo/';
+depth=1
+windowWidths=0 ; %[0 250:50:1000];
+sF = 0.9:0.025:1.1;
+thryFiles = dir('/proj/snic2022-5-384/users/x_albdv/data/CHR/New Ref-Theory files April 2024/*.mat');
+
+
+local_pipeline_mp(ix, iy, dirName, depth, windowWidths, sF, thryFiles)
+
+
